68505bfa1dfdd06f0fbbf543f2e89825dae3c222
Juan Victor <140073835+PortgasDJu4n@users.noreply.github.com>
2025-06-05 23:24:15 -0300

    Questão_01.py

---
+++ Lista_01/Questão_01.py
@@ -0,0 +1,15 @@
+# Questão 01 - Cálculo da Média de Notas
+
+print("  *Calculo da Média de Notas*")
+
+#Entrada das Notas
+nota1 = float(input("Digite a primeira Nota:"))
+nota2 = float(input("Digite a segunda Nota:"))
+nota3 = float(input("Digite a terceira Nota:"))
+nota4 = float(input("Digite a terceira Nota:"))
+
+#Cálculo das Notas
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+#Exibição da Média
+print(f"Sua média de notas é: {media}")
