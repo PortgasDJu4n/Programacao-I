9374873dc22c68c176c917c1340f6bd7beaebae3
Juan Victor <140073835+PortgasDJu4n@users.noreply.github.com>
2025-06-05 23:24:44 -0300

    Create Questão_02.py

---
+++ Lista_01/Questão_02.py
@@ -0,0 +1,14 @@
+# Questão 02 - Cálculo da Área do QUadrado
+
+print("  *Calculo do dobro da área do quadrado*")
+
+#Entrada do dado
+side = float(input("Digite o tamanho do Lado do Quadrado em cm:"))
+
+#Cálculo das Áreas
+area1 = side ** 2 #Área
+area2 = area1 * 2 #Dobro da Área
+
+#Exibição das Áreas
+print(f"Área do quadrado: {area1} cm")
+print(f"Dobro da área do quadrado: {area2} cm")
